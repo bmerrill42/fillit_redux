75686aa494dc99562de87067f14de2fbf81d95f4
Amanda Gifford <agifford@student.42.us.org>
2018-05-12 19:13:55 -0700

    Yahoooooooooo

---
+++ fillit.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agifford <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/12 19:00:11 by agifford          #+#    #+#             */
+/*   Updated: 2018/05/12 19:12:27 by agifford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+#include "libft.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#endif
