0eadb1c355fa28f5975cbc60e492505c7f67fb54
filemaker <filemaker@e1z4r3p13.42.us.org>
2018-05-22 13:12:29 -0700

    add next hash function and refine validation function

--- validation.c
+++ validation.c
@@ -50,6 +50,8 @@ typedef struct s_tracker {
   char** candidate;
   int top;
   int left;
+  int y_hash;
+  int x_hash;
 } t_tracker;
 
 t_tracker *construct(char **read_piece) {
@@ -59,28 +61,30 @@ t_tracker *construct(char **read_piece) {
   ret->candidate = read_piece;
   ret->top = get_top(read_piece);
   ret->left = get_left(read_piece);
+  ret->y_hash = ret->top;
+  ret->x_hash = ret->left;
   return (ret);
 }
 
-int next_hash(t_arr_list *traverse, t_tracker *read_piece, int hash_number) {
+void next_hash(t_tracker *read_piece, int hash_count) {
   int x_offset = read_piece->left;
   int y_offset = read_piece->top;
 
   while (y_offset < 4){
     while (x_offset < 4){
-      if (read_piece->candidate[y_offset][x_offset] == '#') {
-        hash_number--;
-      }
-      if (!hash_number)
+      if (read_piece->candidate[y_offset][x_offset] == '#')
+        hash_count--;
+      if (!hash_count)
         break;
-        x_offset++;
+      x_offset++;
     }
-      if (!hash_number)
-        break;
+    if (!hash_count)
+      break;
     y_offset++;
     x_offset = read_piece->left;
   }
-    return (((char**)traverse->piecearr)[y_offset - read_piece->top][x_offset - read_piece->left] != read_piece->candidate[y_offset][x_offset]);
+  read_piece->y_hash = y_offset;
+  read_piece->x_hash = x_offset;
 }
 
 int hashes(char**piece) {
@@ -99,38 +103,38 @@ int hashes(char**piece) {
   return hashcount;
 }
 
-t_arr_list *validate(t_tracker *read_piece, t_arr_list *rejects) {
-  t_arr_list *traverse;
-  int hash_number = 1;
 
-  int loop = 0;
-  
+void remove_matches(t_tracker *read_piece, t_arr_list *rejects) {
+  t_arr_list *traverse;
   
   traverse = g_valid_pieces->next;
-  if (hashes(read_piece->candidate) != 4)
-    return(NULL);
-  while (g_valid_pieces->next != g_valid_pieces->prev){
-    while(traverse != g_valid_pieces) {
-
-      if  (next_hash(traverse, read_piece, hash_number)){
-        list_add(rejects, traverse);
-        traverse->next->prev = traverse->prev;
-        traverse->prev->next = traverse->next;
-      }
-      traverse = traverse->next;
-    }
-    traverse = g_valid_pieces->next;
-    hash_number++;
-
-
-    printf("loop %d\n", loop++);
+  while(traverse != g_valid_pieces) {
     
+    //remove all the unlike pieces
+    if  (((char**)traverse->piecearr)               \
+         [read_piece->y_hash - read_piece->top]     \
+         [read_piece->x_hash - read_piece->left]    \
+         !=                                         \
+         read_piece->candidate                      \
+         [read_piece->y_hash]                       \
+         [read_piece->x_hash]){
+      list_add(rejects, traverse);
+      traverse->next->prev = traverse->prev;
+      traverse->prev->next = traverse->next;
+    }
+    traverse = traverse->next;
   }
-  if (g_valid_pieces->next->piecearr == NULL)
-    return (NULL);
-  else
-    return (g_valid_pieces->next);
+}
 
+t_arr_list *validate(t_tracker *read_piece, t_arr_list *rejects, int hash_number) {
+  if (g_valid_pieces->next == g_valid_pieces->prev)
+    return (g_valid_pieces->next->piecearr == NULL \
+            ? NULL                                 \
+            : g_valid_pieces->next);
+    next_hash(read_piece, hash_number);
+    remove_matches(read_piece, rejects);
+    hash_number++;
+    return(validate(read_piece, rejects, hash_number));
 }
 
 //testing mains
@@ -143,8 +147,8 @@ int main() {
   char **ret;
   int x = 0;
   int y = 0;
-  char arr[4][4] = {{'#','#','.','.'},                            \
-                    {'.','.','.','#'},                            \
+  char arr[4][4] = {{'.','#','.','.'},                            \
+                    {'.','#','#','.'},                            \
                     {'.','.','#','.'},                            \
                     {'.','.','.','.'}};
   t_arr_list *temp;
@@ -169,9 +173,12 @@ int main() {
   g_valid_pieces = init_global();
   printf("candidates\n");
 
-  x = 0;
-  y = 0;
-  if (validate(construct(ret), rejects) == NULL)
+  if (hashes(ret) != 4)
+    return(0);
+
+
+  
+  if (validate(construct(ret), rejects, 1) == NULL)
     {    printf("INVALID\n"); return 0;}
   temp = g_valid_pieces->next;
   while (temp != g_valid_pieces) {
@@ -186,6 +193,15 @@ int main() {
     ((t_arr_list*)temp->piecearr)->prev->next = ((t_arr_list*)temp->piecearr);
     temp = temp->prev;
   }
+  
+  printf("reconstructed\n");
+    temp = g_valid_pieces->next;
+  while (temp != g_valid_pieces) {
+    print_piecearr(temp);
+    printf("\n");
+    temp = temp->next;
+  }
+  
 }
 ======= end
 
